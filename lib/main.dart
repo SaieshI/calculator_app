392efb9716eb9866dae5d90171397e642d590b04
Saiesh0309 <saiirukulla056@gmail.com>
2025-02-28 14:48:23 -0500

    Implemented Math Logic

--- lib/main.dart
+++ lib/main.dart
@@ -29,38 +29,79 @@ class CalculatorScreen extends StatefulWidget {
 
 class CalculatorScreenState extends State<CalculatorScreen> {
   String display = '';
+  double? operandOne;
+  double? operandTwo;
+  String? operator;
 
   void buttonPressed(String value) {
     setState(() {
-      display = display + value;
+      if (value == '+' || value == '-' || value == '*' || value == '/') {
+        if (operandOne == null) {
+          operandOne = double.tryParse(display);
+          operator = value;
+          display = '';
+        }
+      } else if (value == '=') {
+        if (operandOne != null && operator != null) {
+          operandTwo = double.tryParse(display);
+          if (operandTwo != null) {
+            display = result().toString();
+            operandOne = double.tryParse(display);
+            operandTwo = null;
+            operator = null;
+          }
+        }
+      } else {
+        display = display + value;
+      }
     });
+  }
+
+  double result() {
+    switch (operator) {
+      case '+':
+        return operandOne! + operandTwo!;
+      case '-':
+        return operandOne! - operandTwo!;
+      case '*':
+        return operandOne! * operandTwo!;
+      case '/':
+        return operandTwo != 0 ? operandOne! / operandTwo! : 0;
+      default:
+        return 0;
+    }
   }
 
   void clearDisplay() {
     setState(() {
       display = '';
+      operandOne = null;
+      operandTwo = null;
+      operator = null; 
     });
   }
 
   Widget buildButton(String text) {
     return Expanded(
-      child: InkWell(
-        onTap: () => buttonPressed(text),
-        child: Container(
-          margin: const EdgeInsets.all(8),
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: Colors.orange, 
-            borderRadius: BorderRadius.circular(10), 
-          ),
-          child: Center(
-            child: Text(
-              text, 
-              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-            ),
-          )
+        child: InkWell(
+            onTap: () => buttonPressed(text),
+            child: Container(
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    text,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold
+                      ),
+                  ),
+                )
+              )
         )
-      )
     );
   }
 
@@ -84,10 +125,10 @@ class CalculatorScreenState extends State<CalculatorScreen> {
           const SizedBox(height: 20),
           Column(
             children: [
-              Row(children: [buildButton('7'), buildButton('8'), buildButton('9'), buildButton('/')]), 
-              Row(children: [buildButton('4'), buildButton('5'), buildButton('6'), buildButton('*')]),
-              Row(children: [buildButton('1'), buildButton('2'), buildButton('3'), buildButton('-')]),
-              Row(children: [buildButton('0'), buildButton('+')]),
+              Row(children: [buildButton('7'),buildButton('8'),buildButton('9'),buildButton('/')]),
+              Row(children: [buildButton('4'),buildButton('5'),buildButton('6'),buildButton('*')]),
+              Row(children: [buildButton('1'),buildButton('2'),buildButton('3'),buildButton('-')]),
+              Row(children: [buildButton('0'), buildButton('+'), buildButton('=')]),
             ],
           ),
         ],
